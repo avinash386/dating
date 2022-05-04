0c0f323f1eccbccf4c2a3b4ae7ffbcbad7ee4530
Avinash Dev Nagumanthri <avinash.dev@gmail.com>
2022-05-04 21:50:15 +0530

    Add files via upload
    Added

---
+++ Souvenir.R
@@ -0,0 +1,16 @@
+###################################
+#Souvenir Sales
+#Assignment for LDA
+#Author:Avinash Dev Nagumanthri - PGID - 92210067
+
+#Created on: 4th May 2022
+####################################
+
+#Clear the list of the variables
+rm(list=ls())
+
+#Loading the forecast data
+library(fpp2)
+
+#Load the dataset
+data <-read_excel("/Users/av_sa/Desktop/ISB/Courses/LDA/assignment/dating/souvenir/SouvenirSales.xlsx")
